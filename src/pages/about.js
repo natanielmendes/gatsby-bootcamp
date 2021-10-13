41959f8034f614b5f9b7e24010a0499f0667c984
Nataniel Mendes <natanielmendes@hotmail.com>
2021-10-13 17:22:47 -0500

    Link to Hithub profile

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I currently work full-time on Confidential projects.</p>
+        </div>
+    )
+}
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>The best way to reach me is via <Link to="https://github.com/natanielmendes">Github.</Link></p>
+        </div>
+    )
+}
+
+export default ContactPage;
