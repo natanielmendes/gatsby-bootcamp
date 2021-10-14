43ab44b3eb9d3113bdc8e10f5baa1f5c8c49a02b
Nataniel Mendes <natanielmendes@hotmail.com>
2021-10-14 15:26:28 -0500

    Adding Footer Component to contact page

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Created by Nataniel Carvalho © 2021</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/footer';
 
 const ContactPage = () => {
     return (
         <div>
             <h1>Contact</h1>
             <p>The best way to reach me is via <a href="https://github.com/natanielmendes" target="_blank">Github.</a></p>
+            <Footer />
         </div>
     )
 }
