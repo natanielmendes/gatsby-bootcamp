2981eb744ba40f96a949f833cb2bec03e54f508f
Nataniel Mendes <nataniel.mendes@sfappworks.com>
2021-10-22 21:01:27 -0300

    Adding styles to the website header

--- src/components/header.js
+++ src/components/header.js
@@ -5,21 +5,25 @@ import * as headerStyles from './header.module.scss';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Nataniel Carvalho</h1>
+        <header className={headerStyles.header}>            
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Nataniel Carvalho
+                </Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link className={headerStyles.link} to="/">Home</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
