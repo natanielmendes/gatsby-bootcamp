386223cd82617569d28eb473fb230acfca31647e
Nataniel Mendes <nataniel.mendes@sfappworks.com>
2021-10-24 10:54:49 -0300

    Writing graphql query for Blog posts titles and dates

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                date
+              }
+            }
+          }
+        }
+      }
+    `);
     return (
         <Layout>
             <h1>Blog</h1>
---
+++ src/posts/react.md
@@ -0,0 +1,6 @@
+---
+title: "React"
+date: "10-21-2021"
+---
+
+In this post you'll practice React.
