64b08b89210444c8b2cea2a19abf40676f85fb7a
Nataniel Mendes <natanielmendes@hotmail.com>
2021-11-09 22:14:41 -0300

    About section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
     return (
         <Layout>
             <h1>About Me</h1>
-            <p>I currently work full-time on Confidential widely in-use projects.</p>
+            <p>I currently work full-time on Confidential projects.</p>
             <p><Link to="/contact">Want to work with me? Reach out.</Link></p>
         </Layout>
     )
